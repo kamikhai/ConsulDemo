cc9c84aceccd9c8cd1f3d5a8910c040375b14902
kami_khai <kamilla-h@bk.ru>
2021-06-03 23:08:11 +0300

    add code

---
+++ consul2/src/main/java/com/example/consul/ConsulApplication.java
@@ -0,0 +1,31 @@
+package com.example.consul;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConsulApplication {
+
+    @Value("${my.name}")
+    private String name;
+
+    @Value("${my.surname}")
+    private String surname;
+
+    @GetMapping("/full-name")
+    public String getFullName() {
+        return surname + " " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulApplication.class, args);
+    }
+}
